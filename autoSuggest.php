a7a3dafc7da7b3636e130934b578673d663aaec9
kabirpust <kabir.pust@gmail.com>
2014-04-17 15:00:45 +0600

    Create autoSuggest.php
    This file is used to call the records from database.

---
+++ autoSuggest.php
@@ -0,0 +1,32 @@
+﻿<?php
+   $db = new mysqli('DB_HOST', 'USERNAME' ,'PASSWORD', 'DATABASE_NAME');
+	
+	if(!$db) {
+	
+		echo 'Database connection is not found.';
+	} else {
+	
+		if(isset($_POST['q'])) {
+			$q = $db->real_escape_string($_POST['q']);
+			
+			if(strlen($q) >0) {
+
+				$query = $db->query("SELECT country FROM countries WHERE country LIKE '$q%' LIMIT 12");
+				if($query) {
+				echo '<ul>';
+					while ($result = $query ->fetch_object()) {
+	         			echo '<li onClick="fill(\''.addslashes($result->country).'\');">'.$result->country.'</li>';
+	         		}
+				echo '</ul>';
+					
+				} else {
+					echo 'OOPS, There is a problem :(';
+				}
+			} else {
+				// do nothing
+			}
+		} else {
+			echo 'No direct access to this script!';
+		}
+	}
+?>
